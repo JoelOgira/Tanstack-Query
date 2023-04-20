e73d491ad29f893e38688b2552f623218adab0a5
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-20 23:00:38 +0300

    Created AddPost file

---
+++ src/Components/AddPost.jsx
@@ -0,0 +1,12 @@
+import PostForm from "./PostForm";
+
+const AddPost = () => {
+    return (
+        <div>
+            <h1 className="text-center text-4xl font-bold">EditPost</h1>
+            <PostForm />
+        </div>
+    )
+}
+
+export default AddPost;
