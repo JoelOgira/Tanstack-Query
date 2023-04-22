097e999a75ae352d69269682b0d1f370c063a238
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-22 23:47:13 +0300

    Created edit post page

---
+++ src/Pages/EditPost.jsx
@@ -0,0 +1,12 @@
+import PostForm from "../Components/PostForm";
+
+const EditPost = () => {
+    return (
+        <div>
+            <h1 className="text-center text-4xl font-bold">EditPost</h1>
+            <PostForm />
+        </div>
+    )
+}
+
+export default EditPost;
