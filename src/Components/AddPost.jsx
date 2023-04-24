b7bc4c8f2d58325af24b33a4dee835dcb769daf8
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-24 22:02:30 +0300

    modified Add post page

--- src/Components/AddPost.jsx
+++ src/Components/AddPost.jsx
@@ -3,7 +3,7 @@ import PostForm from "./PostForm";
 const AddPost = () => {
     return (
         <div>
-            <h1 className="text-center text-4xl font-bold">EditPost</h1>
+            <h1 className="text-center text-4xl font-bold">AddPost</h1>
             <PostForm />
         </div>
     )
