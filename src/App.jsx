2b45e29153fba6e470feaf8e6a5d88a429114957
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-24 22:01:29 +0300

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
 import Footer from './Components/Footer';
 import PostLists from './Pages/PostLists';
 import Post from './Pages/Post';
 import EditPost from './Pages/EditPost';
 import ErrorPage from './Pages/ErrorPage';
+import Kicks from './Pages/Kicks';
 
 const App = () => {
   return (
     <div className='App h-[100dvh]'>
+      <Header />
       <Routes>
         <Route path='/' element={ <PostLists /> } />
+        <Route path='/kicks' element={ <Kicks /> }  />
         <Route path='/posts/:id' element={ <Post /> } />
         <Route path='/posts/:id/edit' element={ <EditPost /> } />
         <Route path='*' element={ <ErrorPage /> } />
