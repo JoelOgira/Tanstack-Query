1a1537eae6bd3ceb3c8775d9af69251aa00f1396
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-26 17:35:56 +0300

    Changed styling

--- src/Components/AddPost.jsx
+++ src/Components/AddPost.jsx
@@ -2,7 +2,7 @@ import PostForm from "./PostForm";
 
 const AddPost = () => {
     return (
-        <div>
+        <div className="mt-4">
             <h1 className="text-center text-4xl font-bold">AddPost</h1>
             <PostForm />
         </div>
