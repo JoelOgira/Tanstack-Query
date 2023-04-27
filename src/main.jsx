2c2f91fb737d0e40be9b7bdaf397775a34e51282
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-27 19:11:26 +0300

    Added react devtools

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
+// Create client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <QueryClientProvider client={queryClient}>      
+          <App />      
+          <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>,
 )
