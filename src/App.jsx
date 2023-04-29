07763a15c46e75accda23ada462ff93cb5d90ad4
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-29 20:37:08 +0300

    Added more files for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import PostLists from './Pages/PostLists';
-import Post from './Pages/Post';
-import EditPost from './Pages/EditPost';
-import ErrorPage from './Pages/ErrorPage';
+import TodoList from './features/Todos/TodoList';
 import Kicks from './Pages/Kicks/Kicks';
+import Users from './Pages/Users/Users';
+import UserDetails from './Pages/Users/UserDetails';
+import EditUsers from './Pages/Users/EditUsers';
 
 const App = () => {
   return (
     <div className='App h-[100dvh]'>
       <Header />
       <Routes>
-        <Route path='/' element={ <PostLists /> } />
-        <Route path='/kicks' element={ <Kicks /> }  />
-        <Route path='/posts/:id' element={ <Post /> } />
-        <Route path='/posts/:id/edit' element={ <EditPost /> } />
-        <Route path='*' element={ <ErrorPage /> } />
+        <Route path='/' element={<Kicks />} />
+        <Route path='/todos' element={<TodoList />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/:id' element={<UserDetails />} />
+        <Route path='/users/:id' element={<UserDetails />} />
+        <Route path='/users/edit/:id' element={<EditUsers />} />
       </Routes>
       <Footer/>
     </div>
