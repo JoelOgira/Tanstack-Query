dc8f9664c81a60357344f20bbb7ada041c2f3194
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-04-29 20:34:26 +0300

    created UserDetails file

---
+++ src/Pages/Users/UserDetails.jsx
@@ -0,0 +1,38 @@
+import { useParams,useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from '../../api/usersApi';
+
+const UserDetails = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const { data: user, isLoading, isError, error } = useQuery({
+        queryKey: [ 'user', id ],
+        queryFn: () => getUser(id),
+        enabled: Boolean(id)
+    })
+
+    return (
+        <div className="px-10 my-4 p-4 border border-slate-400 rounded mx-auto md:container">
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && isError && <p>Something went wrong!!! {error.message}</p>}
+            {!isLoading && !isError &&
+                <>
+                    <div className="flex flex-col space-y-2 py-2">
+                        <p>Name: {user?.name}</p>
+                        <p>Details: {user?.details}</p>
+                    </div>
+                    <button onClick={() => navigate(`/users/edit/${user?.id}`)} 
+                    className="border border-black px-4 py-2 hover:bg-black hover:text-white rounded-lg ">
+                        Edit
+                    </button>
+                    <button onClick={() => navigate(`/users/edit/${user?.id}`)} 
+                    className="border bg-black text-white px-4 py-2 hover:bg-white hover:text-black hover:border-black rounded-lg mx-5">
+                        Delete
+                    </button>
+                </>
+            }
+        </div>
+    )
+}
+
+export default UserDetails;
