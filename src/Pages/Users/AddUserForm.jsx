cf468ffb5ac540e9ac6ffd587777726d0e95d153
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-05 03:59:16 +0300

    removed props

--- src/Pages/Users/AddUserForm.jsx
+++ src/Pages/Users/AddUserForm.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { addUser } from '../../api/usersApi';
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const CreateUsers = ({ isLoading, isError, error }) => {
+const CreateUsers = () => {
 
     const [name, setName] = useState('');
     const [details, setDetails] = useState('');
@@ -23,29 +23,28 @@ const CreateUsers = ({ isLoading, isError, error }) => {
 
     return (
         <>
-            {isLoading && <p>Loading...</p>}
-            {!isLoading && isError && <p>Something went wrong!!! {error.message}</p>}
-            {!isLoading && !isError && 
-                <>
-                    <form onSubmit={handleSubmit} className="flex flex-col">
-                        <label htmlFor="name">Username</label>
-                        <input 
-                            type="text" 
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                            className="border border-slate-400 focus:outline-transparent p-2 mb-4 rounded"
-                            placeholder="Username"
-                        />
-                        <label htmlFor="details">Details</label>
-                        <textarea name="details" cols="30" rows="3" className="border border-slate-400 focus:outline-transparent p-2 rounded"
-                        placeholder="Details"
-                        value={details}
-                        onChange={e => setDetails(e.target.value)}
-                        ></textarea>
-                        <button type="submit" className="border px-4 py-2 rounded-lg mx-auto mt-4">Create</button>
-                    </form>
-                </>
-            }
+            <form onSubmit={handleSubmit} className="flex flex-col">
+                <label htmlFor="name">Username</label>
+                <input 
+                    type="text" 
+                    required
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    className="border border-slate-400 focus:outline-transparent p-2 mb-4 rounded"
+                    placeholder="Username"
+                />
+                <label htmlFor="details">Details</label>
+                <textarea name="details" cols="30" rows="3" className="border border-slate-400 focus:outline-transparent p-2 rounded"
+                placeholder="Details"
+                value={details}
+                required
+                onChange={e => setDetails(e.target.value)}
+                ></textarea>
+                <div className="mb-4">
+                    <button type="submit" className="border px-4 py-2 rounded-lg mx-auto mt-4">Create</button>
+                </div>
+            </form>
+            <hr />
         </>
     )
 }
