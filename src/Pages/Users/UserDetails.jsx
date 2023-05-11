d5f8e38bf9d805bf4af1cdb547674cd31b302c44
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-11 07:46:03 +0300

    Modified the file

--- src/Pages/Users/UserDetails.jsx
+++ src/Pages/Users/UserDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams,useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getUser, deleteUser } from '../../api/usersApi';
 
@@ -14,7 +14,7 @@ const UserDetails = () => {
 
     const deleteUserMutation = useMutation(deleteUser, {
         onSuccess: () => {
-            queryClient.invalidateQueries(['user', id]);
+            queryClient.invalidateQueries([ 'user', id ]);
         }
     })
 
@@ -24,16 +24,26 @@ const UserDetails = () => {
             {!isLoading && isError && <p>Something went wrong!!! {error.message}</p>}
             {!isLoading && !isError &&
                 <>
-                    <div className="flex flex-col space-y-2 py-2">
-                        <p>Name: {user?.name}</p>
-                        <p>Details: {user?.details}</p>
+                    <div className="flex flex-col space-y-10 py-2">
+                        <div>
+                            <p className="pb-1 font-semibold text-sm underline">Username</p>
+                            <p className="p-2">{user?.name}</p>
+                        </div>
+                        <div>
+                            <p className="pb-1 font-semibold text-sm underline">Details</p>
+                            <textarea name="details" rows="10" className="border border-slate-400 focus:outline-transparent p-2 rounded w-full"
+                                placeholder="Details"
+                                defaultValue={user?.details}
+                                readOnly
+                            ></textarea>
+                        </div>
                     </div>
-                    <button onClick={() => navigate(`/users/edit/${user?.id}`)} 
-                    className="border border-black px-4 py-2 hover:bg-black hover:text-white rounded-lg ">
+                    <button onClick={() => navigate(`/users/edit/${user?.id}`)}
+                        className="border border-black px-4 py-2 hover:bg-black hover:text-white rounded-lg ">
                         Edit
                     </button>
-                    <button onClick={() => {deleteUserMutation.mutate({id: user?.id}); navigate('/users')}}
-                    className="border bg-black text-white px-4 py-2 hover:bg-white hover:text-black hover:border-black rounded-lg mx-5">
+                    <button onClick={() => { deleteUserMutation.mutate({ id: user?.id }); navigate('/users') }}
+                        className="border bg-black text-white px-4 py-2 hover:bg-white hover:text-black hover:border-black rounded-lg mx-5">
                         Delete
                     </button>
                 </>
